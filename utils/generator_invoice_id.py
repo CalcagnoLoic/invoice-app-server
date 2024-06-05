8c043640c5661744f0c5c7421adb29e7c9d7acf9
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-06-05 16:31:17 +0200

    adding workflow + fixing utils

--- utils/generator_invoice_id.py
+++ utils/generator_invoice_id.py
@@ -10,12 +10,12 @@ def generator_invoice_id() -> string:
     """
 
     letter = string.ascii_uppercase
+    letters = "".join(random.choices(letter, k=2))
+
     number = string.digits
+    numbers = "".join(random.choices(number, k=4))
 
-    letters = "".join([str(random.choices(letter)) for _ in range(2)])
-    numbers = "".join(str(random.choices(number)) for _ in range(4))
-
-    print("{}{}".format(letters, numbers))
     return "{}{}".format(letters, numbers)
+
 
 generator_invoice_id()
